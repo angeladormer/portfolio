4f6222522bac7d0ebd97b8a3770b5436db30c59d
angeladormer <105027604+angeladormer@users.noreply.github.com>
2022-05-31 13:53:06 -0500

    Update to Portfolio

---
+++ script.js
@@ -0,0 +1,13 @@
+var aboutMeHeader = document.querySelector(".aboutMeHeader");
+var projectHeader = document.querySelector(".projectHeader");
+
+
+aboutMeHeader.addEventListener("mousemove", function(){
+    aboutMeHeader.style.animationIterationCount = "1";
+})
+
+projectHeader.addEventListener("mousemove", function(){
+    projectHeader.style.animationIterationCount = "1";
+})
+
+
